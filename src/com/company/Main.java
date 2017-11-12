004ffb5f14451c30d257435dcc1dab6be7e3cd0a
AmazingLu <amazinglu166@gmail.com>
2017-11-11 20:04:55 -0500

    run with ant build

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,11 @@
 package com.company;
 
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
-
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int[] arr = {1,2,3};
+        BinarySearch bs = new BinarySearch(arr, 2);
+        System.out.println(bs.find());
     }
 }
